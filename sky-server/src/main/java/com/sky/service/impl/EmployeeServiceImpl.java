06c7ca60c498600a476647ccc4f397f2fd813663
Hashtag-H <3127287081@q.com>
2025-10-12 10:12:02 +0800

    苍穹外卖项目初始提交10 12

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import static com.alibaba.druid.util.Utils.md5;
+import static org.apache.poi.poifs.crypt.HashAlgorithm.md5;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -39,7 +42,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        //需要进行md5加密，然后再进行比对
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
